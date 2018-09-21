a39b7c9f3159ce25327f6854349905af954199b2
Phucanh April Tran <phucanhapril@gmail.com>
2018-09-21 02:13:43 -0400

    feat: adapt prompt for Parsley

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
-"format cjs";
+'format cjs';
 
 var wrap = require('word-wrap');
 var map = require('lodash.map');
@@ -14,12 +14,11 @@ var filter = function(array) {
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function (options) {
-
+module.exports = function(options) {
   var types = options.types;
 
   var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
+  var choices = map(types, function(type, key) {
     return {
       name: rightPad(key + ':', length) + ' ' + type.description,
       value: key
@@ -39,7 +38,9 @@ module.exports = function (options) {
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      console.log(
+        '\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n'
+      );
 
       // Let's ask some questions of the user
       // so that we can populate our commit
@@ -52,79 +53,82 @@ module.exports = function (options) {
         {
           type: 'list',
           name: 'type',
-          message: 'Select the type of change that you\'re committing:',
+          message: "Select the type of change that you're committing:",
           choices: choices,
           default: options.defaultType
-        }, {
-          type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n',
-          default: options.defaultScope
-        }, {
+        },
+        {
           type: 'input',
           name: 'subject',
-          message: 'Write a short, imperative tense description of the change:\n',
+          message:
+            'Write a short, imperative tense description of the change:\n',
           default: options.defaultSubject
-        }, {
+        },
+        {
           type: 'input',
           name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n',
+          message:
+            'Provide a longer description of the change: (press enter to skip)\n',
           default: options.defaultBody
-        }, {
+        },
+        {
           type: 'confirm',
           name: 'isBreaking',
           message: 'Are there any breaking changes?',
           default: false
-        }, {
+        },
+        {
           type: 'input',
           name: 'breaking',
           message: 'Describe the breaking changes:\n',
           when: function(answers) {
             return answers.isBreaking;
           }
-        }, {
+        },
+        {
           type: 'confirm',
           name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
+          message: 'Does this change affect any open Pivotal tickets?',
           default: options.defaultIssues ? true : false
-        }, {
+        },
+        {
           type: 'input',
           name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+          message: 'Add ticket links:\n',
           when: function(answers) {
             return answers.isIssueAffected;
           },
           default: options.defaultIssues ? options.defaultIssues : undefined
         }
       ]).then(function(answers) {
-
         var maxLineWidth = 100;
 
         var wrapOptions = {
           trim: true,
           newline: '\n',
-          indent:'',
+          indent: '',
           width: maxLineWidth
         };
 
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
         // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        var head = (answers.type + ': ' + answers.subject.trim()).slice(
+          0,
+          maxLineWidth
+        );
 
         // Wrap these lines at 100 characters
         var body = wrap(answers.body, wrapOptions);
 
         // Apply breaking change prefix, removing it if already present
         var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
+        breaking = breaking
+          ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '')
+          : '';
         breaking = wrap(breaking, wrapOptions);
 
         var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
 
-        var footer = filter([ breaking, issues ]).join('\n\n');
+        var footer = filter([breaking, issues]).join('\n\n');
 
         commit(head + '\n\n' + body + '\n\n' + footer);
       });
